33f9467edc16dde3d105365ebffcf9ad34d6a83b
Shantanu Negi <shantanu_negi@hotmail.com>
2026-02-17 12:53:45 +1100

    created unit tests.

---
+++ WebsiteAIAssistant.Tests/WebsiteAIAssistantTests.cs
@@ -0,0 +1,31 @@
+﻿namespace WebsiteAIAssistant.Tests
+{
+    public class WebsiteAIAssistantTests
+    {
+        public WebsiteAIAssistantTests()
+        {
+            
+        }
+
+        [Theory]
+        [InlineData("What are the requisites for carbon credits?", 0)]
+        [InlineData("What is the colour of a rose?", -1)]
+        public async Task ValidatePredictions(string userInput, float expectedResult)
+        {
+            // Arrange
+            // Path to load model
+            string modelPath = Path.Combine(Environment.CurrentDirectory, "SampleWebsite-AI-Model.zip");
+
+            await PredictionEngine.LoadModelAsync(modelPath);
+
+            var input = new ModelInput { Feature = userInput };
+
+            // Act
+            var prediction = await PredictionEngine.PredictAsync(input);            
+
+            // Assert
+            Assert.NotNull(prediction);
+            Assert.Equal(expectedResult, prediction.PredictedLabel);
+        }
+    }
+}
